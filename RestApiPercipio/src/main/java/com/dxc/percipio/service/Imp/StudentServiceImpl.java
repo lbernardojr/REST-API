e22e3a3ef78d59ff4c2e694051cbea572589ee6b
Lauro Bernardo Jr <lauro.bernardojr@dxc.com>
2022-08-30 17:25:05 +0800

    Added logger in student service

--- RestApiPercipio/src/main/java/com/dxc/percipio/service/Imp/StudentServiceImpl.java
+++ RestApiPercipio/src/main/java/com/dxc/percipio/service/Imp/StudentServiceImpl.java
@@ -3,6 +3,8 @@ package com.dxc.percipio.service.Imp;
 import com.dxc.percipio.model.Students;
 import com.dxc.percipio.repository.StudentRepository;
 import com.dxc.percipio.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    private final Logger log = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     @Autowired
     StudentRepository studentRepository;
@@ -20,6 +24,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Students> findAllStudents() {
+        log.info("Fetching all students");
         return studentRepository.findAll();
     }
 
